3cc9e4b6ce8717b400bef5fb0f619724ebc22796
Carl Walsh <darthwalsh@gmail.com>
2016-06-24 22:25:51 -0700

    abstract BinOp

--- java/BinaryOp.java
+++ java/BinaryOp.java
@@ -1,13 +1,9 @@
-public class BinaryOp implements Expression {
+public abstract class BinaryOp implements Expression {
   public Expression e1;
   public Expression e2;
   
   public BinaryOp(Expression newE1, Expression newE2) {
     e1 = newE1;
     e2 = newE2;
-  }
-  
-  public int Evaluate() {
-    throw new UnsupportedOperationException("Can't evaluate a BinaryOp!");
   }
 }
