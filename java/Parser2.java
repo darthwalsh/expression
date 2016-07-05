f596066786316a9338ece388a39a0b42a564b671
Carl Walsh <darthwalsh@gmail.com>
2016-07-04 17:39:13 -0700

    better comments

--- java/Parser2.java
+++ java/Parser2.java
@@ -1,32 +1,39 @@
 import java.util.*;
 import java.util.regex.*;
+
+// Parses calculator input in polish notation
 public class Parser2 {
   public static Expression Parse(String s) {
-    ArrayList<String> lex = Lex(s);
-    Result r = Helper(lex, 0);
-    if(r.end != lex.size())
+    ArrayList<String> tokens = Lex(s);
+    Result r = ParseFrom(tokens, 0);
+    if(r.end != tokens.size())
       throw new UnsupportedOperationException("Expected EOF");
     return r.e;
   }
   
+  // Splits a string of calculator input into a list of tokens without whitespace
+  // e.g. splits "+ 1 2" into ["+", "1", "2"]
   private static ArrayList<String> Lex(String s) {
-    ArrayList<String> lex = new ArrayList<String>();
+    ArrayList<String> tokens = new ArrayList<String>();
     
     Pattern p = Pattern.compile("-?\\d+|[-+*/^%]");
     Matcher m = p.matcher(s);
     
     while(m.find())
-      lex.add(m.group());
+      tokens.add(m.group());
       
-    return lex;
+    return tokens;
   }
   
-  private static Result Helper(ArrayList<String> lex, int i) {
-    String s = lex.get(i);
+  // Parses out an expression from a list of tokens
+  // e.g. if tokens was ["*", "+", "1", "2", "3"] and startFrom was 1, 
+  //      returns { end = 4, e = Sum { Constant { 1 }, Constant { 2 } } }
+  private static Result ParseFrom(ArrayList<String> tokens, int startFrom) {
+    String s = tokens.get(startFrom);
     
     if("-+*/^%".indexOf(s) >= 0) {
-      Result first = Helper(lex, i+1);
-      Result second = Helper(lex, first.end);
+      Result first = ParseFrom(tokens, startFrom+1);
+      Result second = ParseFrom(tokens, first.end);
 
       Expression e;
       char c = s.charAt(0);
@@ -45,12 +52,13 @@ public class Parser2 {
     
     try {
       int n = Integer.parseInt(s);
-      return new Result(i+1, new Constant(n));
+      return new Result(startFrom+1, new Constant(n));
     } catch (NumberFormatException ex) { }
         
-    throw new UnsupportedOperationException("Unknown at: " + lex.get(i) + " #" + i);
+    throw new UnsupportedOperationException("Unknown at: " + s + " #" + startFrom);
   }
 
+  // Helper class that stores the result of parsing: an Expression and how many tokens until the end
   private static class Result {
     public int end;
     public Expression e;
