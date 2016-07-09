5979a8063794646deb408cbc2cde7a715c4f8689
Carl Walsh <darthwalsh@gmail.com>
2016-07-09 13:18:55 -0700

    cpp delegating ctors

--- cpp/math.cpp
+++ cpp/math.cpp
@@ -2,58 +2,30 @@
 
 #include "math.hpp"
 
-sum::sum(expression* left, expression* right)
-: binaryop(left, right)
-{ }
-
 int sum::evaluate()
 {
   return left->evaluate() + right->evaluate();
 }
-
-difference::difference(expression* left, expression* right)
-: binaryop(left, right)
-{ }
 
 int difference::evaluate()
 {
   return left->evaluate() - right->evaluate();
 }
 
-
-product::product(expression* left, expression* right)
-: binaryop(left, right)
-{ }
-
 int product::evaluate()
 {
   return left->evaluate() * right->evaluate();
 }
-
-
-quotient::quotient(expression* left, expression* right)
-: binaryop(left, right)
-{ }
 
 int quotient::evaluate()
 {
   return left->evaluate() / right->evaluate();
 }
 
-
-modulo::modulo(expression* left, expression* right)
-: binaryop(left, right)
-{ }
-
 int modulo::evaluate()
 {
   return left->evaluate() % right->evaluate();
 }
-
-
-power::power(expression* left, expression* right)
-: binaryop(left, right)
-{ }
 
 int power::evaluate()
 {
--- cpp/math.hpp
+++ cpp/math.hpp
@@ -4,36 +4,36 @@
 
 class sum : public binaryop {
   public:
-  sum(expression* left, expression* right);
+  using binaryop::binaryop;
   int evaluate() override;
 };
 
 class difference : public binaryop {
   public:
-  difference(expression* left, expression* right);
+  using binaryop::binaryop;
   int evaluate() override;
 };
 
 class product : public binaryop {
   public:
-  product(expression* left, expression* right);
+  using binaryop::binaryop;
   int evaluate() override;
 };
 
 class quotient : public binaryop {
   public:
-  quotient(expression* left, expression* right);
+  using binaryop::binaryop;
   int evaluate() override;
 };
 
 class modulo : public binaryop {
   public:
-  modulo(expression* left, expression* right);
+  using binaryop::binaryop;
   int evaluate() override;
 };
 
 class power : public binaryop {
   public:
-  power(expression* left, expression* right);
+  using binaryop::binaryop;
   int evaluate() override;
 };
