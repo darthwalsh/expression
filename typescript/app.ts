50adfae3f0b3f8fda7f5e51c7ebfbb7ba4ae34d7
Carl Walsh <darthwalsh@gmail.com>
2016-07-17 23:18:51 -0700

    ts test loop

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,5 +1,32 @@
 /// <reference path="maths.ts" />
 
+declare var require: any;
+
 console.log(new Maths.Sum(
   new Maths.Constant(3), 
   new Maths.Constant(2)).evaluate());
+
+var fs = require("fs");
+
+fs.readFile('..\\test.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var lines = data.split('\r\n');
+  for (var i = 0; i < lines.length; i += 3) {
+    var line = lines[i];
+    console.log(line);
+
+    var expected = +lines[i+1].substring(1);
+
+    //TODO var e = parser.parse(line);
+    var actual = 2 //TODO e.evaluate();
+
+    if (expected !== actual) {
+      console.log("expected: " + expected + " != actual: " + actual);
+      return;
+    }
+
+    console.log('= ' + expected);
+  }
+});
