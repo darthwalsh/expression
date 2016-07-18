cdfe3c6e49123d8b56e041fe7dbfdfb086d6b492
Carl Walsh <darthwalsh@gmail.com>
2016-07-17 23:03:42 -0700

    ts basic expression

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,1 +1,39 @@
-console.log('hello world');
+interface Expression {
+  evaluate() : number
+}
+
+class Constant implements Expression {
+  n: number;
+
+  constructor(n: number) {
+    this.n = n;
+  }
+
+  evaluate() : number {
+    return this.n;
+  }
+}
+
+abstract class BinaryOp implements Expression {
+  left: Expression;
+  right: Expression;
+
+  constructor(left: Expression, right: Expression) {
+    this.left = left;
+    this.right = right;
+  }
+
+  abstract op(x: number, y: number) : number;
+
+  evaluate() : number {
+    return this.op(this.left.evaluate(), this.right.evaluate());
+  }
+} 
+
+class Sum extends BinaryOp {
+  op(x: number, y: number) : number {
+    return x + y;
+  }
+}
+
+console.log(new Sum(new Constant(1), new Constant(2)).evaluate());
