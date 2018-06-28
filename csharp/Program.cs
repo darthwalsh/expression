b653de6c9b2baa25db464e37e0489ee9e11558d0
Carl Walsh <darthwalsh@gmail.com>
2018-06-28 07:40:39 -0700

    Add C# template

---
+++ csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("..\\test.txt");
+            for (var i = 0; i < lines.Length; i += 3) {
+                var line = lines[i];
+                var actual = -1; //TODO
+                Console.WriteLine($"{line} = {actual}");
+
+                var expected = int.Parse(lines[i+1].Substring(1));
+                if (expected != actual) {
+                    Console.Error.WriteLine($"Expected {expected} but got {actual}");
+                }
+            }
+        }
+    }
+}
